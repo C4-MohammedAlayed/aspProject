bae16f59b2def50089803e739b742eecec2b4fa4
Mohammed AL-Ayed <malayed@etech-systems.local>
2022-10-18 17:57:13 +0300

    up

---
+++ WebApplication4/WebApplication4/Controllers/UserController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Model;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        db DB = new db();
+
+        [HttpGet]
+        public List<User> GetUser()
+        {
+            List<User> UserLists = new List<User>();
+            User user = new User();
+            
+            DataSet ds = DB.GetUsers(user);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                UserLists.Add(new User
+                {
+                    id = Convert.ToInt32(dr["id"]),
+                    UserName = dr["UserName"].ToString(),
+                    firstname= dr["firstname"].ToString(),
+                    lastname=dr["lastname"].ToString(),
+                    Password=dr["Password"].ToString(),
+                    dob= dr["dob"].ToString(),
+                    role= dr["role"].ToString(),
+                    contactno= dr["contactno"].ToString(),
+                    gender= dr["gender"].ToString(),
+                    Email = dr["Email"].ToString(),
+                }); 
+            }
+            return UserLists;
+        }
+
+        [HttpGet("{id}")]
+        public List<User> GetUserBYID(int id)
+        {
+            List<User> UserList = new List<User>();
+            User user = new User();
+         
+            user.id = id;
+            DataSet ds = DB.GetUsers(user);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                UserList.Add(new User
+                {
+                    id = Convert.ToInt32(dr["id"]),
+                    UserName = dr["UserName"].ToString(),
+                    firstname = dr["firstname"].ToString(),
+                    lastname = dr["lastname"].ToString(),
+                    Password = dr["Password"].ToString(),
+                    dob = dr["dob"].ToString(),
+                    role = dr["role"].ToString(),
+                    contactno = dr["contactno"].ToString(),
+                    gender = dr["gender"].ToString(),
+                    Email = dr["Email"].ToString(),
+                });
+            }
+            return UserList;
+        }
+
+
+        [HttpPost]
+        public string AddUser([FromBody] User user)
+           
+        {
+           string Message = string.Empty;
+            try
+            {
+                Message= DB.AddUser(user);// this function will return message if success or field
+                 
+            }
+            catch (Exception ex)
+            {
+
+                Message=ex.Message;
+            }
+            return Message;
+        }
+
+    }
+}
---
+++ WebApplication4/WebApplication4/Model/db.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Model
+{
+    public class db
+    {
+        public db()
+        {
+        }
+
+        public db(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+        }
+        public IConfiguration Configuration { get; }
+         SqlConnection con =  new SqlConnection("Server = 192.168.60.146; Database=test;Integrated Security = True");
+
+
+        public string AddUser(User user)
+        {
+            string Message = string.Empty;
+            try
+            {
+                SqlCommand com = new SqlCommand("SpUsers", con);// first parameter is the name of strodProsduer
+
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@id", user.id);
+                com.Parameters.AddWithValue("@UserName", user.UserName);
+                com.Parameters.AddWithValue("@firstName", user.firstname);
+                com.Parameters.AddWithValue("@lastName", user.lastname);
+                com.Parameters.AddWithValue("@Email", user.Email);
+                com.Parameters.AddWithValue("@Password", user.Password);
+                com.Parameters.AddWithValue("@dob", user.dob);
+                com.Parameters.AddWithValue("@role", user.role);
+                com.Parameters.AddWithValue("@contactno", user.contactno);
+                com.Parameters.AddWithValue("@gender", user.gender);
+                
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                Message = "success";
+            }
+            catch (Exception ex)
+            {
+
+             Message=   ex.Message;
+            }
+            finally
+            {
+                if (con.State== System.Data.ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            return Message;
+        }
+
+        // Get
+        public DataSet GetUsers(User user)
+        {
+            string Message = string.Empty;
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlCommand com = new SqlCommand("GetUsers", con);// first parameter is the name of strodProsduer
+
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@id", user.id);
+                com.Parameters.AddWithValue("@UserName", user.UserName);
+                com.Parameters.AddWithValue("@firstName", user.firstname);
+                com.Parameters.AddWithValue("@lastName", user.lastname);
+                com.Parameters.AddWithValue("@Email", user.Email);
+                com.Parameters.AddWithValue("@Password", user.Password);
+                com.Parameters.AddWithValue("@dob", user.dob);
+                com.Parameters.AddWithValue("@role", user.role);
+                com.Parameters.AddWithValue("@contactno", user.contactno);
+                com.Parameters.AddWithValue("@gender", user.gender);
+               
+               
+                 SqlDataAdapter da = new SqlDataAdapter(com);
+                da.Fill(ds);
+                Message = "success";
+               
+            }
+            catch (Exception ex)
+            {
+
+                Message = ex.Message;
+            }
+            return ds;
+        }
+
+    }
+}
